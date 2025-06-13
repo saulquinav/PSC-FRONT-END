b2ccf703cb21fe0c6dffd96ee69c610947472251
aciucaru <aciukru@gmail.com>
2025-06-13 13:55:23 +0300

    Added DTO for Inventory Item

---
+++ src/types/inventory-item/InventoryItemDTO.ts
@@ -0,0 +1,24 @@
+export enum ItemType
+{
+    CPU = "CPU",
+    GPU = "GPU",
+    MOTHERBOARD = "MOTHERBOARD",
+    RAM = "RAM",
+    SSD = "SSD",
+    HDD = "HDD",
+    POWER_SUPPLY = "POWER_SUPPLY",
+    CASE = "CASE",
+    COOLER = "COOLER",
+    OTHER = "OTHER"
+}
+
+export interface InventoryItemDTO
+{
+    id: number;
+    name: string;
+    itemType: ItemType;
+    brand: string;
+    model: string;
+    quantity: number;
+
+}

66f6b34f7c288782898e9e849fadfb4b3fb7e8e6
aciucaru <aciukru@gmail.com>
2025-06-13 22:07:59 +0300

    Added functionality to create an entity on the back-end

--- src/pages/AddInventoryItemPage.tsx
+++ src/pages/AddInventoryItemPage.tsx
@@ -1,8 +1,47 @@
+import { useEffect, useState } from 'react';
 import { useNavigate } from "react-router-dom";
+import axios from 'axios';
+
+import { getBackendBaseApiUrl } from "../service/api-url";
+import { InventoryItemDTO, ItemType } from '../types/inventory-item/InventoryItemDTO';
+
 import "./AddInventoryItemPage.css";
+
+
+const API_URL = getBackendBaseApiUrl() + "/inventoryitems";
 
 export function AddInventoryItemPage() {
   const navigate = useNavigate();
+
+  /* The 'backendAvailable' variable keeps track if the back-end is online (available) or not.
+  ** If the back-end is not available, then this page fails gracefully, instead of trowing
+  ** JavaScript errors in the browser console. */
+  const [backendAvailable, setBackendAvailable] = useState(true);
+  const [errorMessage, setErrorMessage] = useState<string | null>(null);
+
+  const [newItem, setNewItem] = useState<Omit<InventoryItemDTO, 'id'>>({
+        name: "",
+        itemType: ItemType.OTHER,
+        brand: "",
+        model: "",
+        quantity: 0
+    });
+
+  const handleCreate = async () => {
+    try {
+        await axios.post(API_URL, newItem);
+        setNewItem({ name: "", itemType: ItemType.OTHER, brand: "", model: "", quantity: 0 });
+        // fetchItems();
+    }
+    catch (err) {
+        setErrorMessage("Failed to create item.");
+        setBackendAvailable(false);
+    }
+  };
+
+  const handleChange = (field: keyof Omit<InventoryItemDTO, 'id'>, value: any) => {
+      setNewItem(prev => ({ ...prev, [field]: value }));
+  };
 
   return (
     <div className="add-component-container">
@@ -12,25 +51,29 @@ export function AddInventoryItemPage() {
       <form className="component-form">
         <label>
           Name:
-          <input type="text" placeholder="e.g. GeForce RTX 3080" />
+          <input type="text" placeholder="e.g. GeForce RTX 3080" value={newItem.name} onChange={e => handleChange('name', e.target.value)} />
         </label>
+
+        <select value={newItem.itemType} onChange={e => handleChange('itemType', e.target.value as ItemType)} >
+          {Object.values(ItemType).map(type => <option key={type} value={type}>{type}</option>)}
+        </select>
 
         <label>
           Brand:
-          <input type="text" placeholder="e.g. NVIDIA" />
+          <input type="text" placeholder="Brand" value={newItem.brand} onChange={e => handleChange('brand', e.target.value)} />
         </label>
 
         <label>
           Model:
-          <input type="text" placeholder="e.g. Founders Edition" />
+          <input type="text" placeholder="Model" value={newItem.model} onChange={e => handleChange('model', e.target.value)} />
         </label>
 
         <label>
           Quantity:
-          <input type="number" placeholder="e.g. 10" />
+          <input type="number" placeholder="Quantity" value={newItem.quantity} onChange={e => handleChange('quantity', parseInt(e.target.value))} />
         </label>
 
-        <button type="submit" className="submit-button">Add Component</button>
+        <button type="submit" className="submit-button" onClick={handleCreate} >Add Component</button>
       </form>
 
       <button className="back-button" onClick={() => navigate("/dashboard")}>
